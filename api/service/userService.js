a20471f058c114c69b25a9a4cbd54771b5c60c23
mac <mac@macs-MacBook-Air.local>
2023-05-11 23:24:55 +0200

    adding api

---
+++ api/service/userService.js
@@ -0,0 +1,10 @@
+const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' },
+    { id: 3, name: 'Bob Smith' },
+  ];
+  
+  exports.getUsers = () => {
+    return users;
+  };
+  
