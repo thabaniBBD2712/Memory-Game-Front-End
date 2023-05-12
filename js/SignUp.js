7a4623c1d7d17d7ef0e5de2bf6c056471f34d319
Thabang Kubheka <thabangk@bbd.co.za>
2023-05-12 21:39:33 +0200

    Modified form to allow for validation

--- js/SignUp.js
+++ js/SignUp.js
@@ -4,13 +4,34 @@ let signinBtn = document.getElementById("signinBtn");
 let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
 
+function validateSignupForm() {
+	var email = document.getElementById("email").value;
+	var fname = document.getElementById("firstname").value;
+  var lname = document.getElementById("surname").value;
+	var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("password-confirm").value;
 
-function signUp() {
-	alert("Sign up button clicked!");
+	if (email === ""|| lname === ""  || fname === "" || password === ""|| confirmPassword === "" ) {
+		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+		return false;
+	}
+
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+        document.getElementById("errorMsg").innerHTML = "Please enter a valid email address.";
+    return false;
+  }
+
+  if (password !== confirmPassword){
+    document.getElementById("errorMsg").innerHTML = "Passwords do not match"
+		return false;
+  }
+	else if (password.length < 8) {
+		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+		return false;
+	}
+	else {
+		alert("Successfully signed up");
+		return true;
+	}
 }
-
-function signIn() {
-	alert("Sign in button clicked!");
-}
-
-
