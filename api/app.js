e4ce2f3b0a346633e6e7a431edcdc84ec3c00ec1
Thabang Kubheka <thabang.kubheka@vcontractor.co.za>
2023-05-22 18:00:58 +0200

    Added login functionality

--- api/app.js
+++ api/app.js
@@ -3,9 +3,22 @@ const UserRouter = require('./routers/userRouter.js');
 
 
 const server = http.createServer((req, res) => {
-  UserRouter.routeRequest(req, res);
+  if (req.method === 'POST' && req.url === '/signUp.html') {
+    signUpController.handleSignUpRequest(req, res);
+  } else if (req.method === 'GET' && req.url === '/signUp.html') {
+    const filePath = path.join(__dirname, '../../pages/signUp.html');
+    signUpService.serveFile(filePath, 'text/html', res);
+  } else if (req.method === 'GET' && req.url === '/css/signUp.css') {
+    const cssFilePath = path.join(__dirname, '../../css/signUp.css');
+    signUpService.serveFile(cssFilePath, 'text/css', res);
+  } else if (req.method === 'GET' && req.url === '/js/signUpLogic.js') {
+    const jsFilePath = path.join(__dirname, 'js', '../../js/signUpLogic.js');
+    signUpService.serveFile(jsFilePath, 'application/javascript', res);
+  } else {
+    UserRouter.routeRequest(req, res);
+  }
 });
 
-server.listen(3001, () => {
+server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
--- api/routers/userRouter.js
+++ api/routers/userRouter.js
@@ -6,7 +6,6 @@ class UserRouter {
   static async handleRequest(req, res) {
     const reqUrl = url.parse(req.url, true);
 
-   
     if (reqUrl.pathname === '/ola' && req.method === 'GET') {
       await UserController.getUsers(req, res);
     } else if (reqUrl.pathname === '/users' && req.method === 'POST') {
@@ -18,7 +17,16 @@ class UserRouter {
         res.writeHead(500);
         res.end(JSON.stringify({ error: 'Internal server error' }));
       }
-   } else {
+    } else if (reqUrl.pathname === '/login' && req.method === 'POST') {
+      console.log('Received POST request to /login');
+      try {
+        await UserController.loginUser(req, res);
+      } catch (error) {
+        console.error('Error logging in:', error);
+        res.writeHead(500);
+        res.end(JSON.stringify({ error: 'Internal server error' }));
+      }
+    } else {
       res.writeHead(404);
       res.end(JSON.stringify({ error: 'Resource not found' }));
     }
@@ -35,3 +43,4 @@ class UserRouter {
 }
 
 module.exports = UserRouter;
+
