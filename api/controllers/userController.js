6ae14ca06266de9be80e24f808560f8e2c40b20e
Thabang Kubheka <thabang.kubheka@vcontractor.co.za>
2023-05-24 05:40:43 +0200

    Made leader board through dynamic rendering

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,7 +3,7 @@ const { poolPromise, sql } = require('../db/db.js');
 class UserController {
   static async getUsers(req, res) {
     const pool = await poolPromise;
-    const result = await pool.request().query('SELECT * FROM Score');
+    const result = await pool.request().query('SELECT * FROM Score ORDER BY score DESC');
     res.write(JSON.stringify(result.recordset));
     res.end();
   }
