e843746d10ea88fa6d565281d5669c5d18672b0b
thabani <thabani@bbd.co.za>
2023-05-28 22:51:16 +0200

    fixing ui

--- website/js/navbar.js
+++ website/js/navbar.js
@@ -1,19 +1,22 @@
-const loginStatus = localStorage.getItem('userEmail')
+const loginStatus = localStorage.getItem('userEmail');
 const logoutOption = document.getElementById("logout");
+const game = document.getElementById("game");
+const leaderBoard = document.getElementById("leardboard");
+const signUp = document.getElementById("signup");
+const home = document.getElementById("home");
+
 document.addEventListener("DOMContentLoaded", function() {
-  const leaderBoard = document.getElementById("leardboard");
-  const signUp = document.getElementById("signup");
-  
-  if (loginStatus) {
-    logoutOption.style.display = "inline-block";
-    leaderBoard.style.display = "inline-block";
-    signUp.style.display = "none";
-  } else {
-    logoutOption.style.display = "none";
-    leaderBoard.style.display = "none";
-    signUp.style.display = "inline-block";
-  }
+  const displayOption = loginStatus ? "inline-block" : "none";
+  setElementDisplay(logoutOption, displayOption);
+  setElementDisplay(leaderBoard, displayOption);
+  setElementDisplay(game, displayOption);
+  setElementDisplay(signUp, loginStatus ? "none" : "inline-block");
 });
+
 logoutOption.addEventListener('click', function() {
   localStorage.clear();
 });
+
+function setElementDisplay(element, display) {
+  element.style.display = display;
+}
